c15cfa76063cb6eb0c21fb7eab7c5aec57781e1a
ValeriyaVelichko <valeriya.velichk@mail.ru>
2020-07-02 13:49:34 +0300

    Initial commit

---
+++ src/js/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 50,
+    observer: true,
+    observeParents: true,
+    keyboard: {
+        enabled: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+    },
+  });
